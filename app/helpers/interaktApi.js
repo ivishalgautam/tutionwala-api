c5dfbf1d40f4d870a7b0647d7dd5c37b471a3409
Vishal Gautam <ivishalgautamm@gmail.com>
2024-12-10 11:39:37 +0530

    successfully integrated waffly otp send api

--- app/helpers/interaktApi.js
+++ app/helpers/interaktApi.js
@@ -8,7 +8,7 @@ export async function sendOtp({ country_code, mobile_number, fullname, otp }) {
     url: `https://app.wafly.in/api/sendtemplate.php?LicenseNumber=${config.waffly_license_no}&APIKey=${config.waffly_api_key}&Contact=${country_code}${mobile_number}&Template=${config.waffly_template_name}&Param=${fullname},${otp}`,
     headers: {},
   };
-
+  console.log({ config });
   const resp = await axios(configObj);
   console.log({ waffly_resp: JSON.stringify(resp.data) });
   return resp;
