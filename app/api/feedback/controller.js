214995cfa90d64fc69655b076d2b9d1819be3999
Vishal Gautam <ivishalgautamm@gmail.com>
2025-03-01 18:35:35 +0530

    fixed email send error

--- app/api/feedback/controller.js
+++ app/api/feedback/controller.js
@@ -1,7 +1,34 @@
 "use strict";
 import table from "../../db/models.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import ejs from "ejs";
+import { sendMail } from "../../helpers/mailer.js";
+import config from "../../config/index.js";
 
 const create = async (req, res) => {
+  const feedbackTemplatePath = path.join(
+    fileURLToPath(import.meta.url),
+    "..",
+    "..",
+    "..",
+    "..",
+    "views",
+    "feedback.ejs"
+  );
+
+  const feedbackTemplate = fs.readFileSync(feedbackTemplatePath, "utf-8");
+  const feedbackSend = ejs.render(feedbackTemplate, {
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+    phone: req.body.phone,
+    subject: req.body.subject,
+    message: req.body.message,
+  });
+  await sendMail(feedbackSend, config.smtp_from_email);
+
   res.send({ status: true, message: "Thank you for your feedback." });
 };
 
