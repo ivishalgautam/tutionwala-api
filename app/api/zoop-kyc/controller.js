f53bdef93876bf6f3587823d5bf15e53accadd93
Vishal Gautam <ivishalgautamm@gmail.com>
2025-03-08 17:28:20 +0530

    added validation in zoop kyc controller

--- app/api/zoop-kyc/controller.js
+++ app/api/zoop-kyc/controller.js
@@ -3,9 +3,31 @@ import table from "../../db/models.js";
 import axios from "axios";
 import configEnv from "../../config/index.js";
 import { ErrorHandler } from "../../helpers/handleError.js";
+import { z } from "zod";
+
+const zoopOKYCRequestSchema = z.object({
+  customer_aadhaar_number: z.string().min(12).max(12),
+  name_to_match: z.string().optional(),
+  consent: z.literal("Y"),
+  consent_text: z.literal(
+    "I hear by declare my consent agreement for fetching my information via ZOOP API"
+  ),
+});
+
+const zoopOKYCVerifySchema = z.object({
+  request_id: z.string().uuid(),
+  otp: z.string().min(6).max(6),
+  consent: z.literal("Y"),
+  consent_text: z.literal(
+    "I hear by declare my consent agreement for fetching my information via ZOOP API"
+  ),
+});
 
 const aadhaarKYCOTPRequest = async (req, res) => {
   const { role, id } = req.user_data;
+
+  const validateData = zoopOKYCRequestSchema.parse(req.body);
+
   const tutor = await table.TutorModel.getByUserId(req);
   if (role === "tutor" && !tutor)
     return res
@@ -50,6 +72,8 @@ const aadhaarKYCOTPRequest = async (req, res) => {
 
 const aadhaarKYCOTPVerify = async (req, res) => {
   const { role, id } = req.user_data;
+  const validateData = zoopOKYCVerifySchema.parse(req.body);
+
   const tutor = await table.TutorModel.getByUserId(req);
   if (role === "tutor" && !tutor)
     return res
