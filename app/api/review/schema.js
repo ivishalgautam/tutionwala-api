6c0e4a223fd003343dae283b7c83f1154eb1975d
Vishal Gautam <ivishalgautamm@gmail.com>
2025-04-23 15:02:27 +0530

    bug fixes

--- app/api/review/schema.js
+++ app/api/review/schema.js
@@ -5,7 +5,6 @@ export const schema = {
       properties: {
         rating: { type: "integer", minimum: 1 },
         tutor_id: { type: "string", format: "uuid" },
-        enquiry_id: { type: "string", format: "uuid" },
         review: { type: "string", minLength: 3 },
       },
       required: ["rating", "review", "tutor_id", "enquiry_id"],
