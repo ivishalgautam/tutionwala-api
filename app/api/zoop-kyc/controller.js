9ce1bbaf14b462deca766eb70f816f204a04213e
Vishal Gautam <ivishalgautamm@gmail.com>
2025-06-20 17:12:33 +0530

    fixed zoop kyc

--- app/api/zoop-kyc/controller.js
+++ app/api/zoop-kyc/controller.js
@@ -47,15 +47,18 @@ const zoopCallback = async (req, res) => {
   try {
     const request_id = req.body?.request_id;
     const status = req.body?.result?.[0]?.status;
+    console.log({ status });
     if (status === "FETCHED") {
       const zoopRequestRecord =
         await table.ZoopModel.getByRequestId(request_id);
       if (!zoopRequestRecord) return false;
 
+      const user = await table.UserModel.getById(0, zoopRequestRecord.user_id);
+
       const details = transformAadhaarData(req.body);
       req.body.is_aadhaar_verified = true;
       req.body.fullname = details.user_full_name;
-      await table.UserModel.update(req, id);
+      await table.UserModel.update(req, user.id);
       req.body.details = details;
       req.body.customer_aadhaar_number = details.user_aadhaar_number;
 
@@ -78,23 +81,12 @@ const zoopCallback = async (req, res) => {
 };
 
 const zoopRedirect = async (req, res) => {
-  return console.log("Redirect: ", JSON.parse(req.body.payload));
   try {
-    const resp = await axios.request(config);
-    if (resp.data.success) {
-      req.body.is_aadhaar_verified = true;
-      req.body.fullname = resp.data.result.user_full_name;
-
-      await table.UserModel.update(req, id);
-
-      req.body.details = resp.data.result;
-
-      const record = await table.AadhaarModel.getByUserId(id);
-      if (!record) {
-        await table.AadhaarModel.create(req);
-      }
-      res.send({ status: true, message: "Verified." });
-    }
+    console.log(req.body);
+    const request_id = req.body.request_id;
+    console.log({ request_id });
+    await table.ZoopModel.deleteByRequestId(request_id);
+    return true;
   } catch (error) {
     console.log(error);
     ErrorHandler({
